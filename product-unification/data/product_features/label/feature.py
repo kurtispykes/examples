604fe7ba39153073c8d598a577694768a01441d3
Volkan Gurel <vol.gurel@gmail.com>
2021-08-28 19:12:04 -0400

    Remove the product unification example

--- product-unification/data/product_features/label/feature.py
+++
@@ -1,9 +0,0 @@
-from typing import Any
-from layer import Dataset, Context
-
-def build_feature(context: Context, products: Dataset("products")) -> Any:
-
-    df = products.to_spark()
-    feature = df.select("posting_id", "label_group", "image_phash")
-
-    return feature.toPandas()
